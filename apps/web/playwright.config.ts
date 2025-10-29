e24202cc421fd0f19f9c973f9e24023dd3841bd6
Christian Alfoni <christianalfoni@gmail.com>
2025-10-29 19:30:22 +0100

    fix playwright config

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm dev:web",
+    command: "pnpm dev",
     url: "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
   },
