3112159b4e57098d6e6c670d8b483cf51cf2d33b
Christian Alfoni <christianalfoni@christianalfoni.gmail>
2026-04-01 10:55:57 +0200

    remove csb

--- .agent/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export type { SimplifiedEvent, AgentStartedEvent, AgentStatusEvent, AgentTextEvent, AgentThinkingEvent, AgentToolStartEvent, AgentToolCompleteEvent, AgentToolErrorEvent, AgentTodoUpdatedEvent, AgentPermissionRequestedEvent, AgentPermissionRespondedEvent, AgentErrorEvent, AgentCompleteEvent, AgentDiffEvent, FileDiffInfo, InitializeOptions, AgentPort, AgentTask, AgentTaskStatus, AgentInfo, } from './types.js';
-export { AgentClient } from './client-sdk.js';
-export { BASE_CONTEXT, generateSystemPrompt } from './config.js';
-export { AGENT_MODES, generateModeSystemPrompt } from './modes.js';
-export type { AgentModeId, AgentModeConfig } from './types.js';
-//# sourceMappingURL=index.d.ts.map
